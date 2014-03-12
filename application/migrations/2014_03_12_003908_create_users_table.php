7712b7c153aa29a6d6510daad662a71511d97fec
Julio Foulquié <jfoulquie@gmail.com>
2014-03-12 00:43:04 +0000

    Add users table and model.

---
+++ application/migrations/2014_03_12_003908_create_users_table.php
@@ -0,0 +1,32 @@
+<?php
+
+class Create_Users_Table {
+
+	/**
+	 * Make changes to the database.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('users', function($t) {
+			$t->increments('id');
+			$t->string('username', 16);
+			$t->string('password', 64);
+			$t->string('oauth-token', 255);
+			$t->string('oauth-secret', 255);
+			$t->timestamps();
+		});
+	}
+
+	/**
+	 * Revert the changes to the database.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('users');
+	}
+
+}
